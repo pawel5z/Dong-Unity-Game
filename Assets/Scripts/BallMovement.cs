c8cd5ac28a450382221524302c6bcfe8217c9cda
Paweł Szmergała <pawelszme@gmail.com>
2019-07-14 22:33:03 +0200

    Ball resets on goal and launches after 3 s delay Added particle system which activates on each collision with player.

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float initSpeed;
+    public float speed;
+    private readonly List<float> rngPosNegList = new List<float> { -1, 1 };
+    private Rigidbody2D rb2d;
+    private int bounceCount = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+        speed = initSpeed;
+
+        StartCoroutine("Launch");
+    }
+
+    public IEnumerator Launch()
+    {
+        yield return new WaitForSeconds(2);
+        float angleRad = Random.Range(3, 7) * 15 * Mathf.Deg2Rad;
+        rb2d.velocity = new Vector2(initSpeed * Mathf.Sin(angleRad) * rngPosNegList[Random.Range(0, rngPosNegList.Count)], initSpeed * Mathf.Cos(angleRad) * rngPosNegList[Random.Range(0, rngPosNegList.Count)]);
+        yield return null;
+    }
+
+    public void Reset()
+    {
+        this.transform.position = new Vector2(0, 0);
+        this.rb2d.velocity = new Vector2(0, 0);
+        this.speed = initSpeed;
+        this.bounceCount = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Boundary")
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -1 * rb2d.velocity.y);
+        }
+        else if (collision.tag == "Player")
+        {
+            this.GetComponentInChildren<ParticleSystem>().Play();
+            float diff = this.transform.position.y - collision.transform.position.y;
+            float sgn = Mathf.Sign(diff);
+            float intDiff = Mathf.Clamp(Mathf.Floor(diff * sgn), 0, 3);
+            float angleRad = 15 * intDiff * Mathf.Deg2Rad;
+            float xDirSgn = Mathf.Sign(rb2d.velocity.x);
+            float speedMult = 1 + (intDiff / 6);
+
+            speed = initSpeed + 3 * Mathf.Log(2 + bounceCount, 2);
+            rb2d.velocity = new Vector2(-1 * xDirSgn * speed * speedMult * Mathf.Cos(angleRad), sgn * speed * speedMult * Mathf.Sin(angleRad));
+            bounceCount++;
+        }
+    }
+}
