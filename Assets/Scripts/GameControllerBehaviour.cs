e0f05ab02560818c824ead24a2e2578b6ab4ee30
Paweł Szmergała <pawelszme@gmail.com>
2019-07-15 18:33:58 +0200

    Added GameOverPanel Containing two options to choose: restart and quit.

--- Assets/Scripts/GameControllerBehaviour.cs
+++ Assets/Scripts/GameControllerBehaviour.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControllerBehaviour : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+
     public IEnumerator GameOver()
     {
         yield return new WaitForSeconds(3);
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
         Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
