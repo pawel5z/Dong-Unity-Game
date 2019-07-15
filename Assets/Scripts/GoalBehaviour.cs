22da6fabb78002d44f712ede16893cbf63e1b7a4
Paweł Szmergała <pawelszme@gmail.com>
2019-07-15 13:27:10 +0200

    Added particle fx while scoring.

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -1,27 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int score = 0;
+    public Text playerScoreText;
+    public ParticleSystem scoreParticles;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ball")
         {
+            scoreParticles.Play();
             collision.GetComponent<BallMovement>().StartCoroutine("Reset");
             collision.GetComponent<BallMovement>().StartCoroutine("Launch");
+            score++;
+            playerScoreText.text = score.ToString();
         }
     }
 }
