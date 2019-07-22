a4fcb9dc4456e5f1998447b9c47590437394cc0d
Paweł Szmergała <pawelszme@gmail.com>
2019-07-22 21:09:51 +0200

    UI scripts and elements reorganization Removed SelectOnEnable.cs. Replaced by PanelElementSelection which is more gentle. GameOverPanel sprites' swap and resize. Sound fx audio sources' volume lowered.

---
+++ Assets/Scripts/PanelElementSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanelElementSelection : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectedOnInput;
+    public string elementsLayout;
+    private bool selected = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxisRaw(elementsLayout) != 0 && !selected)
+        {
+            eventSystem.SetSelectedGameObject(selectedOnInput);
+            selected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        selected = false;
+    }
+}
